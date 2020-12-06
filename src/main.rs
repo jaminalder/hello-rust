bbf8086af18f66acba5b9c0466895e9292d9de29
taastbeb <benjamin.stalder@swisscom.com>
2020-12-06 16:33:33 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+mod min_winit_example;
+mod image_example;
+
+use std::io::{stdin};
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    // min_winit_example::draw_window();
+    image_example::image_example();
+}
+
+fn guessing() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is: {}", secret_number);
+
+    loop {
+        println!("please input your guess.");
+
+        let mut guess = String::new();
+
+        stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
